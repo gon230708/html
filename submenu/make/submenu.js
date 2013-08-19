48018384d62ca604a547efce2253cb640f05d3f2
gon230708 <str1ker1477@gmail.com>
2013-08-19 21:13:44 +0900

    makeRule

--- submenu/make/submenu.js
+++ submenu/make/submenu.js
@@ -4,5 +4,6 @@ document.write('Contents');
 document.write('</div>');
 document.write('<ul id="lmenu_body">');
 document.write('<li><a href="1_helloMake.html">Hello make</a></li>');
+document.write('<li><a href="2_makeRule.html">makeのルール</a></li>');
 document.write('</ul>');
 document.write('</div>');
